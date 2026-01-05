dfb418895a344a172dd4a7fd9a6119effc7168b9
Daniil Stepanenko <daniil.stepanenko.96@gmail.com>
2026-01-05 18:01:05 +0300

    Made completion::Pair available as an element for IndexSet

--- src/completion.rs
+++ src/completion.rs
@@ -25,7 +25,7 @@ impl<T: AsRef<str>> Candidate for T {
 }
 
 /// Completion candidate pair
-#[derive(Clone)]
+#[derive(Clone, Eq, PartialEq, Hash)]
 pub struct Pair {
     /// Text to display when listing alternatives.
     pub display: String,
